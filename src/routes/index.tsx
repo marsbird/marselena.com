989c459909d1231351dc07cce5b3185342007feb
Marselena Sequoia <109041187+marsbird@users.noreply.github.com>
2024-09-20 21:52:11 -0400

    Offset sequoia and add aria labels

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -12,10 +12,14 @@ export default function Home() {
   return (
     <main class='flex justify-center items-center h-screen'>
       <div>
-        <h1 class='font-satoshi font-bold leading-10 text-5xl'>
-          marselena <br /> sequoia
+        <h1
+          class='font-satoshi font-bold leading-10 text-5xl'
+          aria-label='Marselena Sequoia'
+        >
+          marselena <br />
+          <span class='relative left-[2px]'>sequoia</span>
         </h1>
-        <h2 class='font-satoshi text-xl mt-4'>
+        <h2 class='font-satoshi text-xl mt-4' aria-label='software engineer'>
           software engineer{bool() ? '_' : ''}
         </h2>
       </div>
