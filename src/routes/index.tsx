c5127a38d16caf23d7b7a70a4074e754558f0b47
Marselena Sequoia <109041187+marsbird@users.noreply.github.com>
2024-09-22 22:32:28 -0400

    Add github link to homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,18 +11,33 @@ export default function Home() {
 
   return (
     // slightly bottom-weight the centered div
-    <div class='relative bottom-5'>
-      <h1 class='text-5xl font-bold leading-10' aria-label='Marselena Sequoia'>
-        marselena <br />
-        {/* push sequoia slightly right so that strokes line up perfectly */}
-        <span class='relative left-[2px]'>sequoia</span>
-      </h1>
-      <h2
-        class='relative left-[2px] mt-4 text-xl'
-        aria-label='software engineer'
-      >
-        software engineer{bool() ? '_' : ''}
-      </h2>
+    <div>
+      <div class='relative bottom-5'>
+        <h1
+          class='text-5xl font-bold leading-10'
+          aria-label='Marselena Sequoia'
+        >
+          {/* push "marselena" slightly left so the letters in "sequoia" line up perfectly */}
+          <span class='relative right-[2px]'>marselena</span> <br />
+          sequoia
+        </h1>
+        <h2 class='mt-4 text-xl' aria-label='software engineer'>
+          software engineer{bool() ? '_' : ''}
+        </h2>
+      </div>
+      <a class='mt-3 flex items-center' href='https://github.com/marsbird'>
+        <img
+          class='mr-1 hidden size-3 dark:inline'
+          src='/logos/github-mark-white.svg'
+          alt='GitHub logo'
+        />
+        <img
+          class='mr-1 size-3 dark:hidden'
+          src='/logos/github-mark.svg'
+          alt='GitHub logo'
+        />
+        <p class='font-satoshiItalic text-xs font-light'>marsbird </p>
+      </a>
     </div>
   );
 }
