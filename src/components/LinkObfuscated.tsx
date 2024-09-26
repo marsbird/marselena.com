4e324e0272b850363a3aaaec3f0dd10ef3d40511
Marselena Sequoia <109041187+marsbird@users.noreply.github.com>
2024-09-25 22:25:56 -0400

    Switch a tag to div

--- src/components/LinkObfuscated.tsx
+++ src/components/LinkObfuscated.tsx
@@ -8,7 +8,7 @@ const LinkObfuscated: Component<{
   alt: string;
 }> = (props) => {
   return (
-    <a class='mb-1 flex w-fit items-center'>
+    <div class='mb-1 flex w-fit items-center'>
       <img class='mr-1 size-3 dark:hidden' src={props.src} alt={props.alt} />
       <img
         class='mr-1 hidden size-3 dark:inline'
@@ -24,7 +24,7 @@ const LinkObfuscated: Component<{
         <b class='hidden'>here</b>
         {props.domainLabel}
       </p>
-    </a>
+    </div>
   );
 };
 
