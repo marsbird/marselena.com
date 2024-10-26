431870b5847bda63fc5168c8d516ddbee710affb
Marselena Sequoia <109041187+marsbird@users.noreply.github.com>
2024-10-26 19:52:54 -0400

    Add blinking cursor as island

---
+++ islands/BlinkingCursor.tsx
@@ -0,0 +1,21 @@
+import { signal } from "@preact/signals";
+
+interface Props {
+  text: string;
+  delay: number;
+}
+
+export function BlinkingCursor({ text, delay }: Props) {
+  const cursor = signal("_");
+
+  setInterval(() => {
+    cursor.value = cursor.value ? "" : "_";
+  }, delay);
+
+  return (
+    <>
+      {text}
+      {cursor}
+    </>
+  );
+}
