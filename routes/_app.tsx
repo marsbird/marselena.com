73a561bb25501d9d6ae70b165ad981f928e1bf0c
Marselena Sequoia <109041187+marsbird@users.noreply.github.com>
2024-10-26 23:54:36 -0400

    Add language tag

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,11 +1,11 @@
 import { type PageProps } from "$fresh/server.ts";
 export default function App({ Component }: PageProps) {
   return (
-    <html>
+    <html lang="en">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="theme-color" content="#000000"/>
+        <meta name="theme-color" content="#000000" />
         <meta
           name="description"
           content="software engineer based in lenapehoking, building open-source tools for local community"
