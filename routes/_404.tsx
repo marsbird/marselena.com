d8728237f51d92f4bf4abe0f4dcd8d0bf419ee6a
Marselena Sequoia <109041187+marsbird@users.noreply.github.com>
2024-10-31 19:43:27 -0400

    Add 404 to page not found

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,3 +1,8 @@
 export default function Error404() {
-  return <h1 class="text-xl">page not found</h1>;
+  return (
+    <h1 class="text-center">
+      <p class="text-3xl">404</p>
+      <p class="text-xl">page not found</p>
+    </h1>
+  );
 }
