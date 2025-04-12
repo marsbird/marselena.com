e7ba18bb0cb7f58b82dbdd33b74979886f4f14a0
Marselena Sequoia <109041187+marsbird@users.noreply.github.com>
2025-04-12 16:44:32 -0400

    feat: migrate to astro

--- routes/_404.tsx
+++
@@ -1,8 +0,0 @@
-export default function Error404() {
-  return (
-    <h1 class="text-center">
-      <p class="text-3xl">404</p>
-      <p class="text-xl">page not found</p>
-    </h1>
-  );
-}
---
+++ src/components/Typewriter.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "preact/hooks";
+
+interface TypewriterProps {
+  text: string;
+  cursor?: string;
+  delay?: number;
+  typeSpeed?: number;
+  cursorSpeed?: number;
+}
+
+export default function Typewriter({
+  text,
+  cursor = "│",
+  delay = 400,
+  typeSpeed = 80,
+  cursorSpeed = 750,
+}: TypewriterProps) {
+  const [displayText, setDisplayText] = useState("");
+  const [displayCursor, setDisplayCursor] = useState(cursor);
+  const [i, setI] = useState(0);
+  const [isDelayed, setIsDelayed] = useState(true);
+
+  useEffect(() => {
+    let id: NodeJS.Timeout;
+
+    if (isDelayed) {
+      // on initial load, add a delay
+      id = setTimeout(() => {
+        setIsDelayed(false);
+      }, delay);
+    } else if (i < text.length) {
+      // after initial delay, start typing between 1 and 1.5 typeSpeed
+      const randomSpeed = typeSpeed * (1 + Math.random() * 0.5);
+      id = setTimeout(() => {
+        setDisplayText((prev) => prev + text[i]);
+        setI((prev) => prev + 1);
+      }, randomSpeed);
+    } else {
+      // after typing concludes, blink cursor
+      id = setTimeout(
+        () => setDisplayCursor(displayCursor ? "" : cursor),
+        cursorSpeed,
+      );
+    }
+
+    return () => clearTimeout(id);
+  }, [
+    displayCursor,
+    i,
+    isDelayed,
+    text,
+    cursor,
+    delay,
+    typeSpeed,
+    cursorSpeed,
+  ]);
+
+  return (
+    <>
+      {displayText}
+      {displayCursor}
+    </>
+  );
+}
