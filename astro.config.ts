93865d2d3235e0437be10ce51042d03f5929a701
Marselena Sequoia <109041187+marsbird@users.noreply.github.com>
2025-04-12 16:44:32 -0400

    feat: migrate to astro

---
+++ astro.config.ts
@@ -0,0 +1,10 @@
+import preact from "@astrojs/preact";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [preact()],
+});
